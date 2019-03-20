c8d567ffb137187eec32d361367f5aa8a59970d0
Hiep Nguyen <Hiep.Nguyen@arm.com>
2019-03-20 16:21:15 -0700

    initial sample

---
+++ src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {HttpClientModule} from '@angular/common/http';
+import {StocksService} from './services/stocks.service';
+import { SummaryComponent } from './components/summary/summary.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    SummaryComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
+  ],
+  providers: [
+    StocksService,
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
